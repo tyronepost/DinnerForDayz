e79b3bbd69612a98d689713d9f6338edd1235b13
tyronepost <tyrone.post.a@gmail.com>
2016-06-06 20:41:17 -0400

    Added create method to recipes_controller and included mime_types initializer

--- rails/app/controllers/api/recipe_controller.rb
+++ rails/app/controllers/api/recipe_controller.rb
@@ -7,4 +7,14 @@ class Api::RecipeController < ApplicationController
     @recipe = Recipe.find(params[:id])
     render json: @recipe
   end
+
+  def create
+    Recipe.create(create_params)
+  end
+
+  private
+
+  def create_params
+    ActiveModelSerializers::Deserialization.jsonapi_parse(params, only: [:name, :ingredients, :directions])
+  end
 end
