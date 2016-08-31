16491dd0fb7670ade5616f9776da2a40bf5a0f4b
tyronepost <tyrone.post.a@gmail.com>
2016-08-30 22:03:16 -0400

    removed rails app.  Is pure front end ember app

--- rails/db/seeds.rb
+++
@@ -1,8 +0,0 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-Recipe.create(name: 'tacos', ingredients: 'salt', directions: 'heat oven to 400 degrees')
