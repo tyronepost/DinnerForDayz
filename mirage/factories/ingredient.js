4fe6aed9318b2cd5808a38fedfe1f75a98056088
tyronepost <tyrone.post.a@gmail.com>
2016-09-05 19:55:27 -0400

    refactored ingredient view ui into a component

--- mirage/factories/ingredient.js
+++ mirage/factories/ingredient.js
@@ -5,7 +5,7 @@ export default Factory.extend({
     return faker.list.cycle('ground beef', 'corn tortilla', 'salt', 'pepper', 'chili powder', 'onions', 'garlic')(i);
   },
   amount(i) {
-    return faker.list.cycle('1', '12', '1/2', '1/2', '1/4', '1/2', '1')(i);
+    return faker.list.cycle('1', '12', '0.5', '0.5', '0.25', '0.5', '1')(i);
   },
   unit(i) {
     return faker.list.cycle('LBS', 'count', 'TBSP', 'TBSP', 'TSP', 'CUP', 'TBSP')(i);
