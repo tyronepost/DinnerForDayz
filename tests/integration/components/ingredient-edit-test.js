ba34380a0d4cc66db936c55b3496418ce3acb326
tyronepost <tyrone.post.a@gmail.com>
2016-09-06 22:22:30 -0400

    adding ingredient-edit component

---
+++ tests/integration/components/ingredient-edit-test.js
@@ -0,0 +1,16 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('ingredient-edit', 'Integration | Component | ingredient edit', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{ingredient-edit}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+});
